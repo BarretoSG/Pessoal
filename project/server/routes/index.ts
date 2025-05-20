8d02585be5b24546b9ca894f977591ff512f1b40
BarretoSG <143776646+BarretoSG@users.noreply.github.com>
2025-05-20 17:33:40 -0300

    Delete project directory

--- project/server/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { createReadStream } from 'node:fs'
-import { defineEventHandler, sendStream } from 'h3'
-
-export default defineEventHandler((event) => {
-  return sendStream(event, createReadStream('./public/index.html'))
-})
